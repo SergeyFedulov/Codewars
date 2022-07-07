7eec870ec172d5467a9b837211d47fe139ad6b46
Sergey Fedulov <sergeyfedulov.sfd@gmail.com>
2022-07-07 21:16:00 +0300

    task solution #3

---
+++ ConvertNumberToReversed.js
@@ -0,0 +1,17 @@
+'use strict'
+
+/* 
+Convert number to reversed array of digits.
+
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example(Input => Output):
+348597 => [7,9,5,8,4,3]
+0 => [0]
+
+*/
+
+function digitize(num) {
+    return String(num).split('').reverse('').map(Number);
+}
+
